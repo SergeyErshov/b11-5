889eafaeeb092fa09324950b6ac6ff2ad3eaf15d
SergeyErshov <s.ershov.shoko@gmail.com>
2022-12-27 11:51:11 +0300

    add jobs for creating prod and stat VM for task 11 after webinar

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,26 @@ module "yandex_instance_1" {
   instance_name         = "vm01"
   zone_name             = "ru-central1-a"
 }
+
+module "yandex_instance_2" {
+  source                = "./modules/create_vm"
+  disk_size             = "30"
+  cores                 = "2"
+  memory                = "2"
+  instance_family_image = "centos-7"
+  instance_subnet_name  = "a-ru-central1-a"
+  instance_name         = "vm02-prod"
+  zone_name             = "ru-central1-a"
+}
+
+module "yandex_instance_3" {
+  source                = "./modules/create_vm"
+  disk_size             = "30"
+  cores                 = "2"
+  memory                = "2"
+  instance_family_image = "centos-7"
+  instance_subnet_name  = "a-ru-central1-a"
+  instance_name         = "vm03-state"
+  zone_name             = "ru-central1-a"
+}
+
